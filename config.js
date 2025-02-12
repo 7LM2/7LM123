41fdedf7caa41cdc14049889fd2e0d4d80a3f18a
7LM2 <remawiwaseem7@gmail.com>
2025-02-12 07:53:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,9 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1133910956548833383", ""], 
-  mongodbUri : "mongodb+srv://7LM:7LM@discordbot.g8ejp.mongodb.net/?retryWrites=true&w=majority"
-
-",
-  setupFilePath: './commands/setup.json',
-  commandsDir: './commands',  
+  mongodbUri : "mongodb+srv://7LM:7LM@discordbot.g8ejp.mongodb.net/?retryWrites=true&w=majority",
+setupFilePath: './commands/setup.json',
+  commandsDir: './commands',
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
